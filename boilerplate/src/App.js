d1171ec8c86ae81346c97c4e1a782a4105223f27
hosoon2 <hosoon2@gmail.com>
2021-02-26 00:45:15 +0900

    set effect

--- boilerplate/src/App.js
+++ boilerplate/src/App.js
@@ -1,8 +1,115 @@
+import { useEffect, useReducer } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom"; // https://reactrouter.com/web/guides/quick-start
+//import Button from '@material-ui/core/Button'; // https://material-ui.com/
 import logo from './asset/imgs/logo.svg';
-import './asset/css/App.css';
-import Button from '@material-ui/core/Button';
-import './asset/scss/style.scss';
 import Items from '../../components/Items.tsx';
+import './asset/css/App.css';
+import './asset/scss/style.scss';
+
+
+function Home() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      dispatch({ type: 'tick' });
+    }, 1000);
+    return () => clearInterval(id);
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>{count}</h1>
+      <input value={step} onChange={e => {
+        dispatch({
+          type: 'step',
+          step: Number(e.target.value)
+        });
+      }} />
+    </>
+  );
+}
+
+const initialState = {
+  count: 0,
+  step: 1,
+};
+
+function reducer(state, action) {
+  const { count, step } = state;
+  if (action.type === 'tick') {
+    return { count: count + step, step };
+  } else if (action.type === 'step') {
+    return { count, step: action.step };
+  } else {
+    throw new Error();
+  }
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Topics() {
+  let match = useRouteMatch();
+
+
+  useEffect(() => {
+    /* props 로 받은 값을 컴포넌트의 로컬 상태로 설정
+     * 외부 API 요청 (REST API 등)
+     * 라이브러리 사용 (D3, Video.js 등)
+     * setInterval or setTimeout (timeApi)
+     */
+      console.log('mounted');
+    return () => {
+     /* clear timApi & library */
+      console.log('unmounted');
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul className="topics">
+        <li>
+          <Link to={`${match.url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      {/* The Topics page has its own <Switch> with more routes
+          that build on the /topics URL path. You can think of the
+          2nd <Route> here as an "index" page for all topics, or
+          the page that is shown when no topic is selected */}
+      <Switch>
+        <Route path={`${match.path}/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={match.path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let { topicId } = useParams();
+  return <h3>Requested topic ID: {topicId}</h3>;
+}
 
 function App() {
   return (
@@ -22,8 +129,37 @@ function App() {
           Learn React
         </a>
       </header>
-      <Button variant="contained" color="primary">Hello World</Button>
+      
       <Items/>
+
+      <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+      </Router>
+
     </div>
   );
 }
