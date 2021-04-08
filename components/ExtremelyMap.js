bf2a9f7cc8c043f125e8af72fc2a57fc0c694bff
shuushu <hosoon2@gmail.com>
2021-04-08 19:29:23 +0900

    dev mapLayer

--- components/ExtremelyMap.js
+++ components/ExtremelyMap.js
@@ -14,40 +14,6 @@ const MapContainer = styled.div`
   height: 100%;
 `;
 
-const tempGeoJson = {
-  type: "geojson",
-  data: {
-    type: "Feature",
-    geometry: {
-      type: "Polygon",
-      coordinates: [
-        [
-          [-67.13734351262877, 45.137451890638886],
-          [-66.96466, 44.8097],
-          [-68.03252, 44.3252],
-          [-69.06, 43.98],
-          [-70.11617, 43.68405],
-          [-70.64573401557249, 43.090083319667144],
-          [-70.75102474636725, 43.08003225358635],
-          [-70.79761105007827, 43.21973948828747],
-          [-70.98176001655037, 43.36789581966826],
-          [-70.94416541205806, 43.46633942318431],
-          [-71.08482, 45.3052400000002],
-          [-70.6600225491012, 45.46022288673396],
-          [-70.30495378282376, 45.914794623389355],
-          [-70.00014034695016, 46.69317088478567],
-          [-69.23708614772835, 47.44777598732787],
-          [-68.90478084987546, 47.184794623394396],
-          [-68.23430497910454, 47.35462921812177],
-          [-67.79035274928509, 47.066248887716995],
-          [-67.79141211614706, 45.702585354182816],
-          [-67.13734351262877, 45.137451890638886],
-        ],
-      ],
-    },
-  },
-};
-
 function Map() {
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -60,47 +26,97 @@ function Map() {
         [132, 39],
       ],
     });
+    //shuushu.ak4o6zr0
+    //http://a.tiles.mapbox.com/v4/mapbox.mapbox-streets-v8/14/4823/6160.mvt?access_token=<your access token>
 
-    //shuushu.3q5w0olc
     let hoveredStateId = null;
+    let lastFeatureId;
     map.on("load", () => {
-      var stateDataLayer = map.getLayer("si");
-      console.log(stateDataLayer);
-
-      /* map.addLayer({
-        id: "si",
-        source: "composite",
-        type: "fill",
-        sourceLayer: "sisi-4rfnug",
-        paint: {
-          "fill-color": "red",
-        },
-      }); */
+      map.addSource("시군", {
+        type: "vector",
+        url: "mapbox://mapbox.mapbox-streets-v5,shuushu.ak4o6zr0",
+      });
+      // map.addLayer({
+      //   id: "시군벡터",
+      //   type: "line",
+      //   source: "시군",
+      //   "source-layer": "sisi-61f5i6",
+      //   layout: {
+      //     "line-join": "round",
+      //     "line-cap": "round",
+      //   },
+      //   paint: {
+      //     "line-color": "#ff69b4",
+      //     "line-width": [
+      //       "case",
+      //       ["boolean", ["feature-state", "hover"], false],
+      //       3,
+      //       1,
+      //     ],
+      //   },
+      // });
 
       map.on("mouseenter", "si", function (e) {
         if (e.features.length > 0) {
           console.log(e.features[0]);
-
-          /*  if (hoveredStateId !== null) {
+          /*  map.addSource("TEST", {
+            type: "geojson",
+            data: {
+              type: "Feature",
+              geometry: e.features[0].geometry,
+            },
+          });
+          map.addLayer({
+            id: "outline",
+            type: "line",
+            source: "TEST",
+            layout: {},
+            paint: {
+              "line-color": "#000",
+              "line-width": 3,
+            },
+          }); */
+        }
+      });
+      map.on("mousemove", "si", function (e) {
+        ///
+        //
+        console.log(e);
+        if (e.features.length > 0) {
+          if (hoveredStateId !== null) {
             map.setFeatureState(
-              { source: "si", id: hoveredStateId },
+              {
+                source: "시군",
+                id: hoveredStateId,
+                sourceLayer: "sisi-61f5i6",
+              },
               { hover: false }
             );
           }
           hoveredStateId = e.features[0].id;
           map.setFeatureState(
-            { source: "si", id: hoveredStateId },
+            {
+              source: "시군",
+              id: hoveredStateId,
+              sourceLayer: "sisi-61f5i6",
+            },
             { hover: true }
-          ); */
+          );
         }
       });
-
-      /* map.on("mousemove", function (e) {
-        var features = map.queryRenderedFeatures(e.point);
-        console.log(features);
-        //features[0].layer.
-        map.setPaintProperty("gu", "fill-color", "red");
-      }); */
+      map.on("mouseleave", "si", function () {
+        if (hoveredStateId !== null) {
+          map.setFeatureState(
+            {
+              source: "시군",
+              id: hoveredStateId,
+              sourceLayer: "sisi-61f5i6",
+            },
+            { hover: false }
+          );
+        }
+        hoveredStateId = null;
+      });
     });
 
     //getData();
