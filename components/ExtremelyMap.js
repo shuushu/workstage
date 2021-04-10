e7b0fb1e4aa0312f1048e7ed1e171887096bd4d3
hosoon2 <hosoon2@gmail.com>
2021-04-11 02:48:02 +0900

    표현식 테스트

--- components/ExtremelyMap.js
+++ components/ExtremelyMap.js
@@ -16,38 +16,40 @@ const MapContainer = styled.div`
 function generatorColorSteopExpressions(name, color) {
   const dyColor = (v) => {
     let result;
-    let caclu = Math.round(Number(v) / 550);
+    let caclu = Math.round(Number(v) / 700);
     let value = caclu >= 255 ? 0 : 255 - caclu;
+
     switch (color) {
       case "red":
-        result = `rgba(255,${value},${value},0.5)`;
+        result = `rgb(255,${value},${value})`;
         break;
       case "green":
-        result = `rgba(${value},255,${value},0.5)`;
+        result = `rgb(${value},255,${value})`;
         break;
       case "blue":
-        result = `rgba(${value},${value},255,0.5)`;
+        result = `rgb(${value},${value},255)`;
         break;
       case "yellow":
-        result = `rgba(255,255,${value},0.5)`;
+        result = `rgb(255,255,${value})`;
         break;
       default:
-        result = `rgba(0,0,0,0.5)`;
+        result = `rgba(0,0,0,1)`;
         break;
     }
     return result;
   };
-  const values = ["150000", "100000", "50000", "20000"];
+  const values = ["170000", "150000", "100000", "80000", "60000", "40000", "20000", "10000", "5000", "1000"];
 
   let temp = ["case"];
 
   // 값이 단계가 세분화 되어 표현할 때
   values.forEach((v, i) => {
-    temp.push([">", ["get", name], v]);
+    console.log(name, i, v)
+    temp.push([">=", ["get", name], v]);
     temp.push(dyColor(v));
   });
   //temp.push("black");
-  temp.push(dyColor(50000));
+  temp.push(dyColor(1000));
   return temp;
 }
 
@@ -131,38 +133,54 @@ function drawSI(map, hoveredStateId) {
 function drawGU(map, hoveredStateId) {
   map.addSource("GU", {
     type: "vector",
-    url: "mapbox://mapbox.mapbox-streets-v5,shuushu.9w6nyfe6",
+    url: "mapbox://mapbox.mapbox-streets-v5,shuushu.68k17yc7",
+    promoteId: "SIG_CD"
   });
   const 정당별컬러스텝 = (() => {
     return [
-      ["19대대선_자유한국당홍준표", "red"],
-      ["19대대선_더불어민주당문재인", "blue"],
-      ["19대대선_국민의당안철수", "green"],
-      ["19대대선_정의당심상정", "yellow"],
+      ["19대대선구시군_자유한국당홍준표", "red"],
+      ["19대대선구시군_더불어민주당문재인", "blue"],
+      ["19대대선구시군_국민의당안철수", "green"],
+      ["19대대선구시군_정의당심상정", "yellow"],
     ].map(([정당, 컬러]) => {
       return generatorColorSteopExpressions(정당, 컬러);
     });
   })();
-
   map.addLayer({
     id: "GU-fill-Layer",
     type: "fill",
     source: "GU",
-    "source-layer": "SIGU-4kit8a",
+    "source-layer": "SIGUGUN-4j6g82",
     paint: {
       "fill-color": [
         "case",
-        ["==", ["get", "19대대선_개표결과"], "자유한국당홍준표"],
-        [...정당별컬러스텝[0]],
-        ["==", ["get", "19대대선_개표결과"], "더불어민주당문재인"],
+        ["==", ["get", "19대대선구시군_개표결과"], "자유한국당홍준표"],
+        [
+          "case",
+          ["<", ["get", "19대대선구시군_자유한국당홍준표"], "471"],
+          "yellow",
+          ["<", ["get", "19대대선구시군_자유한국당홍준표"], "12666"],
+          "orange",
+          ["<", ["get", "19대대선구시군_자유한국당홍준표"], "29739"],
+          "green",
+          ["<", ["get", "19대대선구시군_자유한국당홍준표"], "51613"],
+          "pink",
+          ["<", ["get", "19대대선구시군_자유한국당홍준표"], "86026"],
+          "rgb(255,100,100)",
+          ["<", ["get", "19대대선구시군_자유한국당홍준표"], "165781"],
+          "rgb(255,255,255)",
+          "rgb(255,20,20)",
+        ],
+        //[...정당별컬러스텝[0]],
+        ["==", ["get", "19대대선구시군_개표결과"], "더불어민주당문재인"],
         [...정당별컬러스텝[1]],
-        ["==", ["get", "19대대선_개표결과"], "국민의당안철수"],
+        ["==", ["get", "19대대선구시군_개표결과"], "국민의당안철수"],
         [...정당별컬러스텝[2]],
-        ["==", ["get", "19대대선_개표결과"], "정의당심상정"],
+        ["==", ["get", "19대대선구시군_개표결과"], "정의당심상정"],
         [...정당별컬러스텝[3]],
         "black",
       ],
-      "fill-opacity": ["interpolate", ["linear"], ["zoom"], 9, 0.5, 11, 0],
+      "fill-opacity": ["step", ["zoom"], 0.9, 11, 0],
     },
   });
   map.addLayer({
@@ -173,7 +191,7 @@ function drawGU(map, hoveredStateId) {
       "line-join": "round",
       "line-cap": "round",
     },
-    "source-layer": "SIGU-4kit8a",
+    "source-layer": "SIGUGUN-4j6g82",
     paint: {
       "line-color": "#000",
       "line-width": [
@@ -182,17 +200,18 @@ function drawGU(map, hoveredStateId) {
         2,
         0.1,
       ],
-      "line-opacity": ["step", ["zoom"], 1, 9, 0],
+      "line-opacity": ["step", ["zoom"], 1, 11, 0],
     },
   });
   map.on("mousemove", "GU-fill-Layer", function (e) {
+    console.log(e.features[0])
     if (e.features.length > 0) {
       if (hoveredStateId !== null) {
         map.setFeatureState(
           {
             source: "GU",
             id: hoveredStateId,
-            sourceLayer: "SIGU-4kit8a",
+            sourceLayer: "SIGUGUN-4j6g82",
           },
           { hover: false }
         );
@@ -202,7 +221,7 @@ function drawGU(map, hoveredStateId) {
         {
           source: "GU",
           id: hoveredStateId,
-          sourceLayer: "SIGU-4kit8a",
+          sourceLayer: "SIGUGUN-4j6g82",
         },
         { hover: true }
       );
@@ -214,7 +233,7 @@ function drawGU(map, hoveredStateId) {
         {
           source: "GU",
           id: hoveredStateId,
-          sourceLayer: "SIGU-4kit8a",
+          sourceLayer: "SIGUGUN-4j6g82",
         },
         { hover: false }
       );
